91f9b3b61d340367385d8c0a081cd86bb09346d0
Larry Weru <larryw09@gmail.com>
2016-03-16 03:55:02 -0400

    base64, api functionality (needs pre-defined credentials)

--- typemint.js
+++ typemint.js
@@ -4,6 +4,14 @@ if (typeof ROOTURL == "undefined") {
 
 editReady = false;
 
+Base64 = { // http://stackoverflow.com/a/26514148/1937233
+  _keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+  encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},
+  decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},
+  _utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},
+  _utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}
+};
+
 Typemint = {
   editor : {
     init : function(){
@@ -145,30 +153,89 @@ Typemint = {
       editReady = true;
     }
   },
+  savePage : function(newHTML, SHA){
+    jQuery.ajax({
+      url: APIROOT + FILEPATH,
+      type: "PUT",
+      headers: {
+          "Authorization": AUTHTOKEN,
+          "Content-Type": "application/json",
+      },
+      contentType: "application/json",
+      data: JSON.stringify({
+          "message": "update " + Date.now(),
+          "content": Base64.encode(newHTML),
+          "sha": SHA
+      })
+    })
+    .done(function(data, textStatus, jqXHR) {
+        console.log("HTTP Request Succeeded: " + jqXHR.status);
+        console.log(data);
+        document.open('text/html');
+        document.write(newHTML);
+        document.close();
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+    })
+    .always(function() {
+        /* ... */
+    });
+  },
   compileChanges : function(regions){
     /// use the current URL to get the source
 
     $.get(window.location.href , function(src) {
-      // my_var contains whatever that request returned
-      // var $src = src;
 
-      var pageContent =  $('<typemint />',{html:src}); //document.createElement('typemint'); // create virtual div
-      //$(pageContent).html(src); // put the src into this virtual div so that you can modify the src vitually
+      var srcBodyOuter = src.match(/<body[^>]*>[\s\S]*<\/body>/gi)[0]; // find the body element
+      var excludeBodyReg = /\<body[^>]*\>([^]*)\<\/body/m; // exclude opening and closing body tags
 
-      // replace the necessary parts
+      // working with just the body, since that's where the writing happens. the rest should remain intact.
+      var $srcBodyInner = $('<div />',{
+        html: srcBodyOuter.match( excludeBodyReg )[1]
+      })
+
+      // replace the necessary parts inside the body. If a section wasn't changed, it doesn't get overwritten, so in that case the DOM creation is all that effected it.
       for (name in regions) {
         console.log(name);
           if (regions.hasOwnProperty(name)) {
-              $(pageContent).find('[data-tm-name='+ name +']').html(regions[name]);
+              $($srcBodyInner).find('[data-tm-name='+ name +']').html(regions[name]);
           }
       }
 
-      // for now I'm just replacing the page with the new content,
-      // later I'll send this to the API to update the repo and make the changes permanent
+      // console.log("$srcBodyInner:");
+      // console.log($srcBodyInner.html());
 
-      document.open('text/html');
-      document.write($(pageContent).html());
-      document.close();
+      // merge the changes into a new html document
+      var newHTML = src.replace( src.match(/<body[^>]*>[\s\S]*<\/body>/gi)[0], "<body>" + $srcBodyInner.html() + "</body>"  ); //$(pageContent).html();
+      //console.log(newHTML);
+
+      if( ( typeof APIROOT == "undefined" ) || ( typeof FILEPATH == "undefined" ) || ( typeof AUTHTOKEN == "undefined" ) ){
+        // these variables aren't set, so can't use Github's API.
+        document.open('text/html');
+        document.write(newHTML);
+        document.close();
+      }
+      else{
+        // upload to GitHub.
+        // first get the SHA
+        jQuery.ajax({
+            url: APIROOT + FILEPATH,
+            type: "GET",
+        })
+        .done(function(data, textStatus, jqXHR) {
+            console.log("HTTP Request Succeeded: " + jqXHR.status);
+            // console.log(data);
+            // great, now save, given the SHA
+            Typemint.savePage(newHTML, data.sha);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log("HTTP Request Failed");
+        })
+        .always(function() {
+            /* ... */
+        });
+      }
 
     });
   }
